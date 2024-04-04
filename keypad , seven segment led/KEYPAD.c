ede4bc586865e80b4ce964d6a06b6aab6c7f0c74
Amal-Vijay <105409607+Amal-Vijay@users.noreply.github.com>
2024-04-04 18:12:57 +0530

    Add files via upload

---
+++ keypad , seven segment led/KEYPAD.c
@@ -0,0 +1,18 @@
+#include<LPC214x.h>
+#include"keypad_GPIO.h"
+#include"LCD.h"
+#include"keypad2.h"
+int main()
+{
+unsigned char mykey;
+initLCD();
+while(1)
+{
+mykey=getKeypad();
+if(mykey!='x')
+{
+LCDdata(mykey);
+delay();
+}
+}
+}
---
+++ keypad , seven segment led/keypad.d
@@ -0,0 +1,5 @@
+.\keypad.o: KEYPAD.c
+.\keypad.o: C:\Keil\ARM\Inc\Philips\LPC214x.h
+.\keypad.o: keypad_GPIO.h
+.\keypad.o: LCD.h
+.\keypad.o: keypad2.h
