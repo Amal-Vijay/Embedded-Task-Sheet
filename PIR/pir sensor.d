de9472e7dc5ead5caa816612d4f737c9f1c64b57
Amal-Vijay <105409607+Amal-Vijay@users.noreply.github.com>
2024-04-05 12:11:44 +0530

    Add files via upload

---
+++ PIR/pir sensor.d
@@ -0,0 +1,2 @@
+.\pir sensor.o: PIR sensor.c
+.\pir sensor.o: C:\Keil\ARM\Inc\Philips\lpc214x.h
---
+++ Sound Sensor/sound sensor.d
@@ -0,0 +1,2 @@
+.\sound sensor.o: Sound Sensor.c
+.\sound sensor.o: C:\Keil\ARM\Inc\Philips\lpc214x.h
